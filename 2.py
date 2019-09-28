bb8e6eb9f35a24a36183ce95194228f07c849c75
Mathematicienne <kristina.bogacheva.2014@mail.ru>
2019-09-28 23:42:34 +0300

    Задание про прямоугольный треугольник

--- 2.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-a = n*2-1
-lst=[]
-for i in range(1,a+1,2):
-    lst.append(' ' * int((a - i)/2) + '*' * i + ' ' * int((a - i)/2))
-print("\n".join(lst))
--- 3.py
+++
@@ -1,5 +0,0 @@
-n = input()
-if n.find('.')>-1:
-    print('Yes')
-else:
-    print('No')
